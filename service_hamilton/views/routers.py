67c893eef5a4102aa6a4f753c42e89714290cca7
AlexTeng <79433869+ShaomingT@users.noreply.github.com>
2023-04-21 13:11:17 +1000

    only need to pass id to hamilton

--- service_hamilton/views/routers.py
+++ service_hamilton/views/routers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request, current_app, abort
+from flask import Blueprint, jsonify, request, current_app, abort, current_app
 from models.hamilton_ticket import HamiltonTicket
 import uuid
 import psutil
@@ -45,11 +45,31 @@ def generate_ticket(ticket_input, app):
 
 
 
-@hamilton_blueprint.route("/hamilton/tickets", methods=["POST"])
-def print_ticket():
-    ticket_data = request.get_json()
+@hamilton_blueprint.route("/hamilton/tickets/<string:ticket_id>", methods=["POST"])
+def print_ticket(ticket_id):
+    # get tickets data
+    ticket_data = current_app.db_tickets.find_one({"id": ticket_id}, projection={"_id": 0})
+    concert_id = ticket_data['concert']['id']
+    concert_data = current_app.db_concerts.find_one({"id": concert_id}, projection={"_id": 0})
+    user_id = ticket_data['user']['id']
+    user_data = current_app.db_users.find_one({"id": user_id}, projection={"_id": 0})
+
+    # svg payload
+    svg_payload = {
+        "id": ticket_data["id"],
+        "name": user_data["name"],
+        "email": user_data["email"],
+        "concert": {
+            "id": concert_data["id"],
+            "name": concert_data["name"],
+            "date": concert_data["date"],
+            "venue": concert_data["venue"],
+        }
+    }
+
+
     app = current_app._get_current_object()
-    Thread(target=generate_ticket, args=(ticket_data, app)).start()
+    Thread(target=generate_ticket, args=(svg_payload, app)).start()
 
     return jsonify({"status": "The asynchronous request was successfully started."}), 202
 
@@ -81,10 +101,27 @@ def generate_concert(concert_input, app):
         logging.error(e)
         app.db_concerts.update_one({"id": concert_id}, {"$set": {"print_status": "ERROR"}})
 
-@hamilton_blueprint.route("/hamilton/concerts", methods=["POST"])
-def print_concert():
+@hamilton_blueprint.route("/hamilton/concerts/<string:concert_id>", methods=["POST"])
+def print_concert(concert_id):
+    # get concert data
+    concert_data = current_app.db_concerts.find_one({"id": concert_id}, projection={"_id": 0})
+    # calculate purchased seats by count the number of tickets in db_users
+    purchased_count = current_app.db_users.count_documents({"concert.id": concert_id})
+    # svg payload
+    svg_payload = {
+        "id": concert_data["id"],
+        "name": concert_data["name"],
+        "date": concert_data["date"],
+        "venue": concert_data["venue"],
+        "seats": {
+            "max": concert_data["capacity"],
+            "purchased": purchased_count
+        }
+    }
+
+
     concert_data = request.get_json()
     app = current_app._get_current_object()
-    Thread(target=generate_concert, args=(concert_data, app)).start()
+    Thread(target=generate_concert, args=(svg_payload, app)).start()
 
     return jsonify({"status": "The asynchronous request was successfully started."}), 202
