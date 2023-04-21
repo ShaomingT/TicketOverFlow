0179956098f61ba81b97bb9eb87b51392dc12f53
AlexTeng <79433869+ShaomingT@users.noreply.github.com>
2023-04-21 22:31:39 +1000

    fix concert mutiple workers will cause final graph incorrect

--- service_hamilton/views/routers.py
+++ service_hamilton/views/routers.py
@@ -82,6 +82,7 @@ def print_ticket(ticket_id):
 def generate_concert(concert_input, app):
     try:
         with app.app_context():
+
             hamilton_path = app.config["HAMILTON_PATH"]
             concert_id = concert_input["id"]
             logging.debug(msg=f"concert_id: {concert_id}")
@@ -94,17 +95,26 @@ def generate_concert(concert_input, app):
             cmd = f"{hamilton_path} generate seating --input {input_file} --output {output_file}"
             print(cmd)
             logging.info("cmd: " + cmd)
-            try:
-                subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-                with open(f"{output_file}.svg", "r") as f:
-                    svg_content = f.read()
-                app.db_concerts.update_one({"id": concert_id}, {"$set": {"print_status": "PRINTED", "svg": svg_content}})
-                os.remove(input_file)
-                os.remove(output_file + ".svg")
-            except subprocess.CalledProcessError as e:
-                print(e)
-                logging.info(e.output)
-                app.db_concerts.update_one({"id": concert_id}, {"$set": {"print_status": "ERROR"}})
+
+            subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+            with open(f"{output_file}.svg", "r") as f:
+                svg_content = f.read()
+
+            # fetch svg_seat_num from concert_data
+            concert_data = app.db_concerts.find_one({"id": concert_id}, projection={"_id": 0})
+
+            purchased_count = concert_input["seats"]["purchased"]
+
+            # compare the svg_seat_num with the purchased_count, if svg_seat_num > purchased_count, abort, otherwise, generate svg. if svg_seat_num doesn't exist, generate svg
+            if "svg_seat_num" in concert_data:
+                if concert_data["svg_seat_num"] > purchased_count:
+                    abort(400, "The number of seats in the svg file is larger than the number of purchased tickets.")
+                    
+
+            app.db_concerts.update_one({"id": concert_id}, {"$set": {"print_status": "PRINTED", "svg": svg_content, "svg_seat_num": purchased_count}})
+            os.remove(input_file)
+            os.remove(output_file + ".svg")
+
     except Exception as e:
         logging.error(e)
         app.db_concerts.update_one({"id": concert_id}, {"$set": {"print_status": "ERROR"}})
@@ -128,6 +138,10 @@ def print_concert(concert_id):
     }
     print("svg_payload: ", svg_payload)
 
+    # compare the svg_seat_num with the purchased_count, if svg_seat_num > purchased_count, abort, otherwise, generate svg. if svg_seat_num doesn't exist, generate svg
+    if "svg_seat_num" in concert_data:
+        if concert_data["svg_seat_num"] > purchased_count:
+            abort(400, "The number of seats in the svg file is larger than the number of purchased tickets.")
 
     concert_data = request.get_json()
     app = current_app._get_current_object()
