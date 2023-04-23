fdc49fe4ac089dcaf3e0c9b41b9ef58a96c58b23
AlexTeng <79433869+ShaomingT@users.noreply.github.com>
2023-04-23 21:02:58 +1000

    init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,12 @@
-echo "assume that it is running on Python3.10 and psgcopg2 installed"
+#!/bin/bash
+
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "|     CSSE6440 CLOUD INFRA ASSIGNMENT     |"
+echo "|          Shaoming Teng, 44660145        |"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "> Before begin to depoly, please make sure the following requirements are met:"
+echo -e "> Please make sure \033[1m\033[31mPython3.10\033[0m and python library \033[1m\033[31mpsycopg2\033[0m is installed on local machine, they are used to upload init data."
+echo ""
+read -p "Press any key to continue... " -n1 -s
+echo ""
+echo "Continuing..."
