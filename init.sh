f17b05f27742eb0b51d4b41cf78b00d8eebce9de
AlexTeng <79433869+ShaomingT@users.noreply.github.com>
2023-04-24 13:42:08 +1000

    init init.sh

--- init.sh
+++ init.sh
@@ -5,8 +5,15 @@ echo "|     CSSE6440 CLOUD INFRA ASSIGNMENT     |"
 echo "|          Shaoming Teng, 44660145        |"
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "> Before begin to depoly, please make sure the following requirements are met:"
-echo -e "> Please make sure \033[1m\033[31mPython3.10\033[0m and python library \033[1m\033[31mpsycopg2\033[0m is installed on local machine, they are used to upload init data."
+echo -e "> Please make sure \033[1m\033[31mPython3.10\033[0m and Python library \033[1m\033[31mpsycopg2\033[0m is installed on local machine, they are used to upload init data to database."
 echo ""
 read -p "Press any key to continue... " -n1 -s
 echo ""
 echo "Continuing..."
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "> Zip lambda function"
+cd ./lambda_hamilton || exit
+# zip folder bin and psycopg2 and file lambda_function to lambda_function.zip
+zip -r lambda_function_payload.zip bin/ psycopg2/ lambda_function.py
+cd ..
+echo "> Generated lambda_hamilton/lambda_function_payload.zip"
