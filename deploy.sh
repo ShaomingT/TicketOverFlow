9e5d27b8c222d70933ff54bf2e84fe7dc9ff2be2
AlexTeng <79433869+ShaomingT@users.noreply.github.com>
2023-05-16 15:37:52 +1000

    update deploy.sh for testing

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,19 @@ echo "> Before begin to depoly, please make sure the following requirements are 
 echo -e "> Please make sure \033[1m\033[31mPython3.10\033[0m, Python library \033[1m\033[31mpsycopg2\033[0m and \033[1m\033[31mDocker\033[0m are installed on local machine, they are used to upload init data to database."
 echo ""
 #read -p "Press any key to continue... " -n1 -s
+echo "installing dependencies..."
+apt update -y
+apt install python3 -y
+apt install python3-pip -y
+python3 -m pip install psycopg2-binary
+
 echo ""
 echo "Continuing..."
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "> Zip lambda function"
 cd ./lambda_hamilton || exit
 # zip folder bin and psycopg2 and file lambda_function to lambda_function.zip
-zip -r lambda_function_payload.zip bin/ psycopg2/ lambda_function.py
+#zip -r lambda_function_payload.zip bin/ psycopg2/ lambda_function.py
 cd ..
 echo "> Generated lambda_hamilton/lambda_function_payload.zip"
 echo "Copying credential file to terraform"
